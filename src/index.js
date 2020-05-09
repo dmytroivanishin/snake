36ab5acb5a4198fb3bb5eedb8174dee533519327
Dima Iv <ivdim4ik95@gmail.com>
2020-05-09 15:46:57 +0300

    Refactoring key control

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import animateRAFInterval from "./utils/animateRAFInterval";
-import moveSnake, { setDirection } from "./utils/moveSnake";
+import { moveSnake, changeDirection } from "./utils/moveSnake";
 
 const state = {
     snake: {
@@ -19,29 +19,20 @@ window.addEventListener("load", () => {
     const canvas = document.getElementById("game-field");
     const ctx = canvas.getContext("2d");
 
-    // console.log(ctx);
-
-    // ctx.fillStyle = "blue";
-    // ctx.beginPath();
-    // ctx.moveTo(6 - (20 / 2), 6 - (20 / 2));
-    // ctx.lineTo(6 + (20 / 2), 6 - (20 / 2));
-    // ctx.lineTo(6 + (20 / 2), 6 + (20 / 2));
-    // ctx.lineTo(6 - (20 / 2), 6 + (20 / 2));
-    // ctx.closePath();
-    // ctx.fill();
-
     document.addEventListener("keydown", (e) => {
 
-        setDirection(e.keyCode, state.snake);
-        moveSnake(state.snake)
-        console.dir(state.snake.direction, state.snake.tail);
+        changeDirection(e.keyCode, state.snake);
+        //moveSnake(state.snake)
+        
+        
+        
         
     });
 
-    // animateRAFInterval.start((time) => {
-    //     moveSnake(state.snake);
-    //     console.dir(state.snake);
-    // }, 1000);
+    animateRAFInterval.start((time) => {
+        moveSnake(state.snake);
+        console.dir(state.snake.direction, state.snake.tail);
+    }, 3000);
 
 
 });
--- src/utils/moveSnake.js
+++ src/utils/moveSnake.js
@@ -13,6 +13,16 @@ const getHeadSnake = (snake) => {
     return snake.tail[snake.tail.length - 1];
 };
 
+const hasDirection = (snake, direction) => {
+    const headSnake = getHeadSnake(snake);
+
+    if(direction === headSnake.d){
+        return false;
+    }
+
+    return true;
+};
+
 const hasSetDirection = (snake, direction) => {
     const headSnake = getHeadSnake(snake);
 
@@ -28,23 +38,12 @@ const hasSetDirection = (snake, direction) => {
     return false;
 }
 
-export const setDirection = (keyCode, snake) => {
-    const direction = mapCode(keyCode);
-    const lastDirection = snake.direction;
-
-    if(hasSetDirection(snake, direction)) {
-        snake.direction = direction;
-    }
-    else{
-        snake.direction = lastDirection;
-    }
-};
-
-export default (snake) => {
+export const moveSnake = (snake) => {
     const headSnake = getHeadSnake(snake);
     const direction = snake.direction;
     let newMovementSnake;
 
+    
     if(direction === "left"){
         newMovementSnake =  { x: headSnake.x - 1, y: headSnake.y, d: direction, h: true };
     }
@@ -68,4 +67,23 @@ export default (snake) => {
 };
 
 
+
+export const changeDirection = (keyCode, snake) => {
+    const direction = mapCode(keyCode);
+    const lastDirection = snake.direction;
+
+    if(!hasDirection(snake, direction)){
+        return false;
+    }
+
+    if(hasSetDirection(snake, direction)) {
+        snake.direction = direction;
+    }
+    else{
+        return false;
+    }
+
+    moveSnake(snake);
+
+};
 
